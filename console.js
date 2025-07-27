159b745c85d217aa3430a58adddf03f752f9383a
Alexander <heinzosesrunge@gmail.com>
2025-07-27 13:56:46 -0400

    adds other console methods

--- console.js
+++ console.js
@@ -1,3 +1,4 @@
+/*
 console.log("un print normal");
 console.info("Console info. Similar a log pero para mostrar información");
 console.warn("Console warn. Para Advertencias");
@@ -18,3 +19,33 @@ for (let i = 0; i < 5000; i++) {
     //Ejecución
 }
 console.timeEnd('operación');
+*/
+
+//Counting
+console.count('contador');
+console.count('contador');
+console.count('contador');
+console.count('contador');
+console.count('contador');
+console.countReset('contador');
+console.count('contador');
+
+//Grouping
+console.group('main');
+console.log('info 1');
+console.group('secondary');
+console.log('secondary info 1');
+console.log('secondary info 2');
+console.groupEnd();
+console.groupEnd();
+
+//Assertions (testing)
+console.assert(1 === 1, 'It will not be shown');
+console.assert(1 === 2, 'It will be shown because its false');
+
+
+//Clearing
+console.clear();
+
+//Tracing
+console.trace("It shows the actual trace");
